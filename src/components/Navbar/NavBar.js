53c49c5078a19f9f9b0d54901cad66ba064832b5
Jose Becerra <jose.antonio23b@gmail.com>
2023-07-29 16:58:04 -0300

    correcion carpeta/imagenes

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import logo from '../../components/img/logo.png'
+import logo from '../../assets/img/logo.png'
 import CartWidget from './CartWidget';
 
 
